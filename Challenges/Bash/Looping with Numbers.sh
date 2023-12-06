ba1a0949b1e3c4de77f78bc3e7143604175ec1a4
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-12-06 23:28:37 +0800

    Update Looping with Numbers.sh

--- Challenges/Bash/Looping with Numbers.sh
+++ Challenges/Bash/Looping with Numbers.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-for i in {1..50}
+for i in $(seq 1 1 50)
 do
     echo $i
 done
+
+/* for loop variable i in the sequence of numbers starting from 1, with a step size of 1 up to 50
+ * beginning of loop
+ * print current value of i as standard output
+ * end of loop */
