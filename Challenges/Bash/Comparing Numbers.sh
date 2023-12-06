43fd42c6fcc99440a6c3c7ffcd11a25cedb1ebe1
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-12-06 23:31:32 +0800

    Update Comparing Numbers.sh

--- Challenges/Bash/Comparing Numbers.sh
+++ Challenges/Bash/Comparing Numbers.sh
@@ -4,14 +4,10 @@ read Y
 if (( $X > $Y ))
 then
     echo "X is greater than Y"
-fi
-
-if (( $X == $Y))
+elif (( $X == $Y))
 then
     echo "X is equal to Y"
-fi
-
-if(( $X < $Y))
+else
 then
     echo "X is less than Y"
 fi
